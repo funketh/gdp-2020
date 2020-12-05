c5bfec5fa80f37bccf3c1de3e382df77c8f4dc1d
funketh <theodor.k.funke@gmail.com>
2020-12-05 15:14:58 +0100

    naming convention

--- U2/KVNummerPruefer.java
+++ U2/KVNummerPruefer.java
@@ -1,26 +1,26 @@
 public class KVNummerPruefer {
     public static void main(String[] args) {
-        String ensurance_num = args[0];
+        String ensuranceNum = args[0];
 
         // only the base number of the input
-        String base_num = ensurance_num.substring(0, ensurance_num.length() - 1);
+        String baseNum = ensuranceNum.substring(0, ensuranceNum.length() - 1);
         // the input control number we will test
-        int control_num = int_value(ensurance_num.charAt(ensurance_num.length() - 1));
+        int controlNum = intValue(ensuranceNum.charAt(ensuranceNum.length() - 1));
 
-        int my_control_num = calc_control_num(base_num);
+        int myControlNum = calcControlNum(baseNum);
 
-        if (my_control_num == control_num)
+        if (myControlNum == controlNum)
             System.out.println("Korrekt");
         else
-            System.out.println("Inkorrekt\nKorrekte Ziffer: " + my_control_num);
+            System.out.println("Inkorrekt\nKorrekte Ziffer: " + myControlNum);
     }
 
-    public static int calc_control_num(String base_num) {
+    public static int calcControlNum(String baseNum) {
         int[] weights = {7, 3, 1};
         int sum = 0;
 
-        for (int i = 0; i < base_num.length(); i++) {
-            int value = int_value(base_num.charAt(i));
+        for (int i = 0; i < baseNum.length(); i++) {
+            int value = intValue(baseNum.charAt(i));
             int weight = weights[i % weights.length];
             sum += value * weight;
         }
@@ -28,9 +28,9 @@ public class KVNummerPruefer {
         return (sum / 10 * 10 + 10 - sum) % 10;
     }
 
-    public static int int_value(char c) {
+    public static int intValue(char c) {
         if      (+c >= 48 && 57 >= +c) return c - 48; // numerical digit
         else if (+c >= 65 && 90 >= +c) return c - 55; // capital letter
-        else throw new InvalidArgumentException("Invalid digit: " + c);
+        else throw new IllegalArgumentException("Invalid digit: " + c);
     }
 }
