ba44ae8ed89fe89ed09b78f0427b13d74a82a5d1
funketh <theodor.k.funke@gmail.com>
2020-12-13 22:13:11 +0100

    finish P2

--- P2/Nolindrom.java
+++ P2/Nolindrom.java
@@ -1,3 +1,5 @@
+import java.math.BigInteger;
+
 public class Nolindrom {
     public static void main(String[] args) {
         if (args.length < 1) {
@@ -7,7 +9,52 @@ public class Nolindrom {
 
         int limit = Integer.parseInt(args[0]);
 
-        long N = 1;
+        if (args.length < 2 || !args[1].equals("x"))
+            printNolindromes(limit);
+        else
+            checkNolindromeOverflows(limit);
+    }
+
+    public static void printNolindromes(int limit) {
+        N_loop: for (long N = 1; N <= limit; N++) {
+            long currentN = N;
+            long R = reverseNum(currentN);
+
+            // Check if the sum would overflow
+            while (currentN < Long.MAX_VALUE - R) {
+                currentN += R;
+                R = reverseNum(currentN);
+
+                // Check if the sum is a palindrome
+                if (currentN == R)
+                    continue N_loop;
+            }
+
+            System.out.println(N);
+        }
+    }
+
+    public static void checkNolindromeOverflows(int limit) {
+        for (long N = 1; N <= limit; N++) {
+            BigInteger currentN = BigInteger.valueOf(N);
+            BigInteger R = reverseNum(currentN);
+
+            for (int i = 1; i <= 100; i++) {
+                currentN = currentN.add(R);
+                R = reverseNum(currentN);
+
+                // Check if the sum is a palindrome
+                if (currentN.compareTo(R) == 0) {
+                    if (currentN.compareTo(BigInteger.valueOf(Long.MAX_VALUE)) <= 0)
+                        break; // Continue if the sum didn't overflow
+
+                    System.out.println(N + " braucht " + i + " Iterationen bis zum Palindrom " + currentN);
+                    return;
+                }
+            }
+        }
+
+        System.out.println("alle Zahlen werden auch durch Abbruch per Ueberlauf gefunden");
     }
 
     public static long reverseNum(long x) {
@@ -15,6 +62,15 @@ public class Nolindrom {
         while (x != 0) {
             r = (r * 10) + (x % 10);
             x /= 10;
+        }
+        return r;
+    }
+
+    public static BigInteger reverseNum(BigInteger x) {
+        BigInteger r = BigInteger.ZERO;
+        while (x.compareTo(BigInteger.ZERO) != 0) {
+            r = r.multiply(BigInteger.TEN).add(x.mod(BigInteger.TEN));
+            x = x.divide(BigInteger.TEN);
         }
         return r;
     }
