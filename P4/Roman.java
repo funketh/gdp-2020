660e8748513649a141a4c29f97d7107ea303d157
funketh <theodor.k.funke@gmail.com>
2021-01-24 21:10:32 +0100

    ...

--- P4/Roman.java
+++ P4/Roman.java
@@ -8,13 +8,14 @@ class Roman {
             System.exit(1);
         }
         int N = Integer.parseInt(args[0]);
-        if (! (1 <= N && N <= 5000)) {
+        if (!(1 <= N && N <= 5000)) {
             System.out.println("Die Zahl muss zwischen 1 und 5000 liegen.");
             System.exit(1);
         }
         System.out.println(roman(N));
     }
 
+    // wrapper function starting recursion
     static String roman(int n) {
         return romanHelper(n, romanNumerals.length - 1);
     }
@@ -22,35 +23,36 @@ class Roman {
     static String romanHelper(int n, int i) {
         int b = romanNumerals[i];
 
+        // calculate how often b fits into n
         int r = n / b;
-        String str;
-        if (i != romanNumerals.length - 1)
-            if (r == 4) {
-                str = romanLetters[i] + romanLetters[i + 1];
-                if (b == 1)
-                    return str;
-                return str + romanHelper(n % b, i - 1);
-            }
-            else if (r == 1) {
-                int nextN = n % b;
-                int nextB = romanNumerals[i - 1];
-                int nextR = nextN / nextB;
-                if (nextR == 4) {
-                    str = romanLetters[i - 1] + romanLetters[i + 1];
-                    if (b == 5)
-                        return str;
-                    return str + romanHelper(nextN % nextB, i - 2);
-                }
-            }
 
-        str = repeat(romanLetters[i], r);
-        if (b == 1)
+        String str = repeat(romanLetters[i], r);
+        
+        // reduce number 
+        n = n % b;
+
+        // check whether four successive letters need to be replaced
+        // then check whether five + four successive letters need to be replaced
+        if (b != 1000 && r == 4) {
+            str = romanLetters[i] + romanLetters[i + 1];
+            if (b == 1)
+                return str;
+            return str + romanHelper(n % b, i - 1);
+        } else if (i > 1 && i % 2 == 0 && n / romanNumerals[i - 2] == 9) {
+            b = romanNumerals[i - 2];
+            str += romanLetters[i - 2] + romanLetters[i];
+            if (b == 1)
+                return str;
+            return str + romanHelper(n % b, i - 2);
+        } else if (b == 1) {
             return str;
-        return str + romanHelper(n % b, i - 1);
+        }
+        return str + romanHelper(n, i - 1);
     }
 
     static String repeat(String str, int n) {
-        if (n == 0) return "";
+        if (n == 0)
+            return "";
         return str + repeat(str, n - 1);
     }
 }
